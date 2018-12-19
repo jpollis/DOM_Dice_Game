999db0ddedf4bd0137b27cf4ad2e51624e50c97b
Joe Willy <34250239+jpollis@users.noreply.github.com>
2018-12-19 16:19:03 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScores, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var global = document.querySelector('#score-0').textContent;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+	
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+		
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+		
+	document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	// generate a random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+	
+	// display result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+	// If the score is not a 1, update round score
+	if (dice !== 1) {
+		//add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//change player
+		nextPlayer();
+	}
+});
+
+
+document.querySelector('.btn-hold').addEventListener("click", function() {
+	// add current score to global score.
+	scores[activePlayer] += roundScore;
+	
+	// update the UI
+	document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+	nextPlayer();
+	
+	// check if the player won the game
+	var playerOneScore = document.getElementById('score-0').textContent;
+    var playerTwoScore = document.getElementById('score-1').textContent;
+	if (playerOneScore >= 100) {
+		document.getElementById('name-0').innerHTML = "Player 1 wins!";
+	} else if (playerTwoScore >= 100) {
+		document.getElementById('name-1').innerHTML = "Player 2 wins!";
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
