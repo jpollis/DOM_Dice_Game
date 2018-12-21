d6bc8d373e194b4f6658cabcb569c1d86004b9b2
Joe Willy <34250239+jpollis@users.noreply.github.com>
2018-12-21 14:30:42 -0500

    more dry and debugged initializer

--- app.js
+++ app.js
@@ -9,11 +9,36 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer;
 
-var scores, roundScores, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+function startGame() {
+  // setting scores to 0
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+  
+  // setting dice and scoreboard to 0
+  noDice();
+  clearScore();
+  clearGlobal();
+  
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  
+  // removing winner and active classes from previous game
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  
+  // adding active class back to player 1 for new game
+  document.querySelector('.player-0-panel').classList.add('active');
+  
+  // enabling roll and hold buttons
+  document.querySelector('.btn-roll').disabled = false;
+  document.querySelector('.btn-hold').disabled = false;
+}
 
 function noDice() {
 	document.querySelector('.dice').style.display = 'none';
@@ -29,10 +54,6 @@ function clearGlobal() {
 	document.getElementById('score-1').textContent = '0';
 }
 
-noDice();
-clearGlobal();
-clearScore();
-
 function nextPlayer() {
 	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 	roundScore = 0;
@@ -41,6 +62,9 @@ function nextPlayer() {
 	document.querySelector('.player-1-panel').classList.toggle('active');
 	noDice();
 }
+
+//Game Begins
+startGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	// generate a random number
@@ -74,6 +98,7 @@ document.querySelector('.btn-hold').addEventListener("click", function() {
 		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+		// disabling hold and roll buttons... end of game
 		document.querySelector('.btn-roll').disabled = true;
 		document.querySelector('.btn-hold').disabled = true;
 		noDice();
@@ -82,20 +107,8 @@ document.querySelector('.btn-hold').addEventListener("click", function() {
 	}
 });
 
-document.querySelector('.btn-new').addEventListener('click', function() {
-	scores = [0,0];
-	roundScore = 0;
-	clearScore();
-	clearGlobal();
-	noDice();
-	document.querySelector('.btn-roll').disabled = false;
-	document.querySelector('.btn-hold').disabled = false;
-	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
-	document.querySelector('#name-' + activePlayer).textContent = "Player " + (activePlayer + 1)
-	document.querySelector('.player-0-panel').classList.add('active');
-});
-
-
+// new game 
+document.querySelector('.btn-new').addEventListener('click', startGame);
 
 
 
